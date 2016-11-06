c4d067ea52ab55b06cefcfdaa02f0f9e17334e97
Rosie Highsmith <ariel.highsmith@gmail.com>
2016-11-06 17:22:45 +0000

    Add new exercise form

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,4 +2,18 @@ class ExercisesController < ApplicationController
   def index
     @exercises = Exercise.all
   end
+
+  def new
+    @exercise = Exercise.new
+  end
+
+  def create
+    Exercise.create(exercise_params)
+    redirect_to root_path
+  end
+
+  private
+  def exercise_params
+    params.require(:exercise).permit(:title, :description)
+  end
 end
